1ccc4eebc5ecb73354e11c1949602e3eb8f738c0
SlytherinBrowser <146814140+SlytherinBrowser@users.noreply.github.com>
2025-09-07 23:06:54 +0200

    Delete page-index.js

--- page-index.js
+++
@@ -1,3 +0,0 @@
-import { auth } from './firebase-init.js';
-import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js';
-onAuthStateChanged(auth, (u)=>{ if(u) location.replace('feed.html'); else location.replace('login.html'); });
