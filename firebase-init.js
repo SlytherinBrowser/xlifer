824f7542dd02c4b2b9884004b14bc4855c67e24a
SlytherinBrowser <146814140+SlytherinBrowser@users.noreply.github.com>
2025-09-07 19:57:59 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -4,14 +4,15 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.4/firebas
 
 export const firebaseConfig = {
   // 🔧 ZAMENJAJ z vrednostmi iz Firebase Console → Project settings → SDK setup & config
-  apiKey: "YOUR_apiKey",
-  authDomain: "YOUR_authDomain",
-  projectId: "YOUR_projectId",
-  storageBucket: "YOUR_storageBucket",
-  messagingSenderId: "YOUR_messagingSenderId",
-  appId: "YOUR_appId"
+  apiKey: "AIzaSyAhA1s2iiJDMMeqoOrp1asJO1eha_R0ivg",
+  authDomain: "slychat1-b76db.firebaseapp.com",
+  databaseURL: "https://slychat1-b76db-default-rtdb.firebaseio.com",
+  projectId: "slychat1-b76db",
+  storageBucket: "slychat1-b76db.firebasestorage.app",
+  messagingSenderId: "508879069664",
+  appId: "1:508879069664:web:ce963d111297cd3480cb85",
+  measurementId: "G-D34XJNNGNQ"
 };
-
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
